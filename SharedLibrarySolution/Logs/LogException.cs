c54e9b676dc2779560d9b58ca4221dcbc3063eb7
qtrungle1012 <trung.lq@isdcorp.vn>
2025-10-16 11:38:13 +0700

    update gitignore

---
+++ SharedLibrarySolution/Logs/LogException.cs
@@ -0,0 +1,19 @@
+﻿using Serilog;
+
+namespace SharedLibrarySolution.Logs
+{
+    public static class LogException
+    {
+        public static void LogError(Exception ex)
+        {
+            LogToFile(ex.Message);
+            LogToConsole(ex.Message);
+            LogToDebugger(ex.Message);
+        }
+        public static void LogToFile(string message) => Log.Information($"{DateTime.Now}: {message}");
+
+        private static void LogToConsole(string message) => Log.Information($"{DateTime.Now}: {message}");
+
+        private static void LogToDebugger(string message) => Log.Information($"{DateTime.Now}: {message}");
+    }
+}
